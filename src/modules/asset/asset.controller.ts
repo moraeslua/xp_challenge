2530ffd85f22adf98296566d01425baac1c4dfc3
moraes <moraeslua66@gmail.com>
2022-07-19 17:00:32 -0300

    feat(assetModule): implements controller layer

--- src/modules/asset/asset.controller.ts
+++ src/modules/asset/asset.controller.ts
@@ -1,16 +1,27 @@
 import { Request, Response } from 'express';
 import { AssetService } from './asset.service';
-import { IGetByAssetRequest } from './interfaces/controller/get-by-asset.request';
 import 'express-async-errors';
+import { IGetByAssetRequest } from './interfaces/asset.controller.interface';
+import { HttpStatus } from 'src/helpers';
+import { IGetAllByAccountInput } from './interfaces/asset.service.interface';
 
 export class AssetController {
   constructor(private assetService: AssetService) {}
 
-  public findById = async (req: Request, res: Response) => {
-    const { assetId } = req.params as unknown as IGetByAssetRequest;
-    const response = await this.assetService.findById({
-      assetId: Number(assetId),
+  public findById = async (req: Request, res: Response): Promise<Response> => {
+    const { id: assetId } = req.params as unknown as IGetByAssetRequest;
+    const response = await this.assetService.getById({ id: Number(assetId) });
+    return res.status(HttpStatus.OK).json(response);
+  };
+
+  public getAllByAccount = async (
+    req: Request,
+    res: Response,
+  ): Promise<Response> => {
+    const { accountId } = req.params as unknown as IGetAllByAccountInput;
+    const response = await this.assetService.getAllByAccount({
+      accountId: Number(accountId),
     });
-    res.status(200).json(response);
+    return res.status(HttpStatus.OK).json(response);
   };
 }
