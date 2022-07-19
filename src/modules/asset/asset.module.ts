0be604a44dc1995b339145503cec340bf6b6f825
moraes <moraeslua66@gmail.com>
2022-07-19 17:01:26 -0300

    feat(assetModule): implements module to export all layers

--- src/modules/asset/asset.module.ts
+++ src/modules/asset/asset.module.ts
@@ -1,11 +1,11 @@
 import { PrismaClient } from '@prisma/client';
 import { AssetController } from './asset.controller';
 import { AssetService } from './asset.service';
-import { IAssetRepository } from './interfaces/repository/get-by-asset.data';
+import { IAssetRepository } from './interfaces/asset.repository.interface';
 import { PrismaAssetRepository } from './repository/prisma.asset.repository';
 
 export class AssetModule {
-  private assetRepository: IAssetRepository;
+  public assetRepository: IAssetRepository;
   private assetService: AssetService;
   public assetController: AssetController;
 
