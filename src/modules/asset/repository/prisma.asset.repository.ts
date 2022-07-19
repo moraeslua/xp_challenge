fea4fd00cd407d56c71716a49f28c4d1dfa982c2
moraes <moraeslua66@gmail.com>
2022-07-19 16:59:53 -0300

    feat(assetModule): implements prisma repository implementation

--- src/modules/asset/repository/prisma.asset.repository.ts
+++ src/modules/asset/repository/prisma.asset.repository.ts
@@ -1,32 +1,47 @@
 import { PrismaClient } from '@prisma/client';
 import {
   IAssetRepository,
-  IGetByAssetData,
+  IGetAssetByIdData,
+  IGetAssetByIdResult,
   IGetByClientData,
-} from '../interfaces/repository/get-by-asset.data';
-import {
-  IGetByAssetResult,
   IGetByClientResult,
-} from '../interfaces/repository/get-by-asset.result';
+  IUpdateAssetAmountData,
+  IUpdateAssetAmountResult,
+} from '../interfaces/asset.repository.interface';
 
 export class PrismaAssetRepository implements IAssetRepository {
   constructor(private client: PrismaClient) {}
-  async findByAsset({ assetId }: IGetByAssetData): Promise<IGetByAssetResult> {
-    const response = await this.client.asset.findUnique({
-      where: { id: assetId },
-    });
 
-    return response;
+  public async updateAmount(
+    data: IUpdateAssetAmountData,
+  ): Promise<IUpdateAssetAmountResult> {
+    const { id, amount } = data;
+    const result = await this.client.asset.update({
+      where: { id },
+      data: { amount },
+    });
+    console.log('update asset result:', result);
+    return result;
   }
 
-  async findByClient({
+  public async findById({
+    id,
+  }: IGetAssetByIdData): Promise<IGetAssetByIdResult> {
+    const result = await this.client.asset.findUnique({
+      where: { id },
+    });
+
+    return result;
+  }
+
+  public async getAllByAccount({
     accountId,
   }: IGetByClientData): Promise<IGetByClientResult> {
-    const response: IGetByClientResult = await this.client
+    const result: IGetByClientResult = await this.client
       .$queryRaw`SELECT i."accountId", i."assetId", i."amount", a.price FROM "Investment" as i 
       LEFT JOIN "Asset" as a ON a.id = i."assetId" 
       WHERE "accountId" = ${accountId} `;
 
-    return response;
+    return result;
   }
 }
