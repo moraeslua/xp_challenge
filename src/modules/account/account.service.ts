e538dc6b562c3dbb5b80079b4506395c676b8eef
moraes <moraeslua66@gmail.com>
2022-07-19 16:53:44 -0300

    feat(accountModule): implements service with get by id, deposit and withdraw

--- src/modules/account/account.service.ts
+++ src/modules/account/account.service.ts
@@ -1,23 +1,88 @@
-import { ICreateAccountInput } from './interfaces/service/create-account.input';
+import { AccountEventType } from '@prisma/client';
+import { HttpException, HttpStatus } from 'src/helpers';
+import { IAccountRepository } from './interfaces/account.repository.interface';
+import {
+  IAccountService,
+  IDepositInput,
+  IDepositOutput,
+  IGetByIdInput,
+  IGetByIdOutput,
+  IWithdrawInput,
+  IWithdrawOutput,
+} from './interfaces/account.service.interface';
+import { AccountValidator } from './validator/account-validator';
 
-export class AccountService {
-  create(createAccountDto) {
-    return 'This action adds a new account';
+export class AccountService implements IAccountService {
+  constructor(private accountRepository: IAccountRepository) {}
+
+  public async getById({ id }: IGetByIdInput): Promise<IGetByIdOutput> {
+    await AccountValidator.getById({ id });
+
+    const result = await this.accountRepository.getById({ id });
+
+    if (!result) {
+      throw new HttpException('Account not found', HttpStatus.NOT_FOUND);
+    }
+
+    delete result.hashedPassword;
+
+    return result;
   }
 
-  findAll() {
-    return `This action returns all account`;
+  public async depositOnAccount(data: IDepositInput): Promise<IDepositOutput> {
+    const { id, value } = data;
+    await AccountValidator.depositOnAccount({ id, value });
+
+    const account = await this.accountRepository.getById({
+      id,
+    });
+
+    if (!account) {
+      throw new HttpException('Account does not exists.', HttpStatus.NOT_FOUND);
+    }
+
+    const newBalance = account.balance + value;
+
+    const result = await this.accountRepository.executeAccountTransaction({
+      accountId: id,
+      value,
+      balance: newBalance,
+      type: AccountEventType.DEPOSIT,
+    });
+
+    return result;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} account`;
-  }
+  public async withdrawFromAccount(
+    data: IWithdrawInput,
+  ): Promise<IWithdrawOutput> {
+    const { id, value } = data;
+    await AccountValidator.withdrawFromAccount({ id, value });
 
-  update(id: number) {
-    return `This action updates a #${id} account`;
-  }
+    const account = await this.accountRepository.getById({
+      id,
+    });
 
-  remove(id: number) {
-    return `This action removes a #${id} account`;
+    if (!account) {
+      throw new HttpException('Account does not exists.', HttpStatus.NOT_FOUND);
+    }
+
+    const newBalance = account.balance - value;
+
+    if (newBalance < 0) {
+      throw new HttpException(
+        'Such amount is not available on account',
+        HttpStatus.FORBIDDEN,
+      );
+    }
+
+    const result = await this.accountRepository.executeAccountTransaction({
+      accountId: id,
+      value,
+      balance: newBalance,
+      type: AccountEventType.WITHDRAW,
+    });
+
+    return result;
   }
 }
