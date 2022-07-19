4932b1ad1b58d59d2d91981f287801af2dcf4c47
moraes <moraeslua66@gmail.com>
2022-07-19 16:56:29 -0300

    feat(accountModule): implements module with all layers

--- src/modules/account/account.module.ts
+++ src/modules/account/account.module.ts
@@ -1,3 +1,18 @@
+import { PrismaClient } from '@prisma/client';
+import { AccountController } from './account.controller';
+import { AccountService } from './account.service';
+import { IAccountRepository } from './interfaces/account.repository.interface';
+import { IAccountService } from './interfaces/account.service.interface';
+import { PrismaAccountRepository } from './repository/prisma.account.repository';
+
 export class AccountModule {
+  public accountRepository: IAccountRepository;
+  private accountService: IAccountService;
+  public accountController: AccountController;
   //
+  constructor(client: PrismaClient) {
+    this.accountRepository = new PrismaAccountRepository(client);
+    this.accountService = new AccountService(this.accountRepository);
+    this.accountController = new AccountController(this.accountService);
+  }
 }
