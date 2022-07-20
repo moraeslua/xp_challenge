b061d7d408ea6e61c7abbf4af996a8e877b0ceda
moraes <moraeslua66@gmail.com>
2022-07-19 21:32:19 -0300

    chore(investmentModule): change dependencies from private to protected

--- src/modules/investment/investment.routes.ts
+++ src/modules/investment/investment.routes.ts
@@ -1,10 +1,22 @@
 import { InvestmentController } from './investment.controller';
 import express from 'express';
+import { ValidateAccount } from 'src/middlewares/validate-account';
 
 export class InvestmentRoutes {
   public routes = express.Router();
-  constructor(protected investmentController: InvestmentController) {
-    this.routes.post('/buy', investmentController.buyStock);
-    this.routes.post('/sell', investmentController.sellStock);
+  constructor(
+    protected investmentController: InvestmentController,
+    protected validateAccount: ValidateAccount,
+  ) {
+    this.routes.post(
+      '/buy',
+      validateAccount.execute,
+      investmentController.buyStock,
+    );
+    this.routes.post(
+      '/sell',
+      validateAccount.execute,
+      investmentController.sellStock,
+    );
   }
 }
