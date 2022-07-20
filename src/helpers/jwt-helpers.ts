326e6472fb57172ee5b78ea94abf6307fec29f78
moraes <moraeslua66@gmail.com>
2022-07-19 21:23:41 -0300

    chore: install and config jwt helpers

---
+++ src/helpers/jwt-helpers.ts
@@ -0,0 +1,14 @@
+import { SignOptions } from 'jsonwebtoken';
+import jwt from 'jsonwebtoken';
+
+const SECRET = process.env.JWT_SECRET || 'jwtSecret';
+
+const jwtConfig: SignOptions = {
+  expiresIn: '25m',
+  algorithm: 'HS256',
+};
+
+export const generateToken = (payload: object): string =>
+  jwt.sign(payload, SECRET, jwtConfig);
+
+export const verifyToken = (token: string) => jwt.verify(token, SECRET);
