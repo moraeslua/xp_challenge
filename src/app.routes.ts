fb355fb9805c2d253858f4b7cf5b37b243ce6ade
moraes <moraeslua66@gmail.com>
2022-07-19 21:33:21 -0300

    feat: instantiate auth module and routes on app

--- src/app.routes.ts
+++ src/app.routes.ts
@@ -6,13 +6,21 @@ import { AccountModule } from './modules/account/account.module';
 import { AccountRoutes } from './modules/account/account.routes';
 import { InvestmentModule } from './modules/investment/investment.module';
 import { InvestmentRoutes } from './modules/investment/investment.routes';
+import { AuthModule } from './modules/auth/auth.module';
+import { AuthRoutes } from './modules/auth/auth.routes';
+import { ValidateAccount } from './middlewares/validate-account';
 const prismaClient = new PrismaClient();
+
+const validateAccount = new ValidateAccount();
 
 const assetModule = new AssetModule(prismaClient);
 const assetRoutes = new AssetRoutes(assetModule.assetController);
 
 const accountModule = new AccountModule(prismaClient);
-const accountRoutes = new AccountRoutes(accountModule.accountController);
+const accountRoutes = new AccountRoutes(
+  accountModule.accountController,
+  validateAccount,
+);
 
 const investmentModule = new InvestmentModule(
   prismaClient,
@@ -22,10 +30,15 @@ const investmentModule = new InvestmentModule(
 
 const investmentRoutes = new InvestmentRoutes(
   investmentModule.investmentController,
+  validateAccount,
 );
+
+const authModule = new AuthModule(accountModule.accountRepository);
+const authRoutes = new AuthRoutes(authModule.authController);
 
 export const appRoutes = express.Router();
 
 appRoutes.use('/asset', assetRoutes.routes);
 appRoutes.use('/account', accountRoutes.routes);
 appRoutes.use('/investment', investmentRoutes.routes);
+appRoutes.use('/auth', authRoutes.routes);
