aa7efe11b0517cdb48809919cfed6639d915a3b2
moraes <moraeslua66@gmail.com>
2022-07-19 21:31:10 -0300

    feat(authModule): implements auth controller

---
+++ src/modules/auth/auth.controller.ts
@@ -0,0 +1,28 @@
+import { Request, Response } from 'express';
+import { HttpStatus } from 'src/helpers';
+import {
+  ISignInRequest,
+  ISignUpRequest,
+} from './interfaces/auth.controller.interface';
+import { IAuthService } from './interfaces/auth.service.interface';
+
+export class AuthController {
+  constructor(private authService: IAuthService) {}
+
+  public signUp = async (req: Request, res: Response) => {
+    const { email, fullName, birthDate, password } = req.body as ISignUpRequest;
+    const response = await this.authService.signUp({
+      email,
+      fullName,
+      birthDate,
+      password,
+    });
+    return res.status(HttpStatus.CREATED).json(response);
+  };
+
+  public signIn = async (req: Request, res: Response) => {
+    const { email, password } = req.body as ISignInRequest;
+    const response = await this.authService.signIn({ email, password });
+    return res.status(HttpStatus.OK).json(response);
+  };
+}
