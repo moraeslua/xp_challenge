3453dbedad488d64d725ed673a96c806e1f9aea2
moraes <moraeslua66@gmail.com>
2022-07-21 12:29:29 -0300

    test(authService): implements 'sign in' tests

--- src/modules/auth/auth.service.spec.ts
+++ src/modules/auth/auth.service.spec.ts
@@ -155,4 +155,139 @@ describe('Auth Service tests', () => {
       });
     });
   });
+  describe('"sign in" method', () => {
+    describe('when sucessfull', () => {
+      const accountGetByEmailMock = jest.fn().mockResolvedValue({
+        id: 4,
+        email: 'rocco@email.com',
+        fullName: 'Rocco Anderson',
+        birthDate: '2000-03-05T03:00:00.000Z',
+        hashedPassword: 'hashedPassword',
+        balance: 0,
+      });
+
+      beforeEach(() => {
+        clearMocks();
+        accountRepositoryMock.getByEmail = accountGetByEmailMock;
+        jest
+          .spyOn(bcrypt, 'compare')
+          .mockImplementation(() => Promise.resolve(true));
+        jest.spyOn(jwtHelpers, 'generateToken').mockReturnValue('token');
+      });
+
+      it('should call repository and helpers with correct arguments', async () => {
+        const result = await authService.signIn({
+          email: 'rocco@email.com',
+          password: 'password',
+        });
+
+        expect(result).toBeDefined();
+        expect(bcrypt.compare).toHaveBeenCalledTimes(1);
+        expect(bcrypt.compare).toHaveBeenCalledWith(
+          'password',
+          'hashedPassword',
+        );
+        expect(jwtHelpers.generateToken).toHaveBeenCalledTimes(1);
+        expect(jwtHelpers.generateToken).toHaveBeenCalledWith({
+          id: 4,
+          email: 'rocco@email.com',
+        });
+      });
+
+      it('should return an object with correct properties and values', async () => {
+        const result = await authService.signIn({
+          email: 'rocco@email.com',
+          password: 'password',
+        });
+
+        expect(result).toEqual(
+          expect.objectContaining({
+            id: 4,
+            email: 'rocco@email.com',
+            fullName: 'Rocco Anderson',
+            birthDate: '2000-03-05T03:00:00.000Z',
+            balance: 0,
+            token: 'token',
+          }),
+        );
+
+        expect(result).not.toHaveProperty('hashedPassword');
+        expect(result).not.toHaveProperty('password');
+      });
+    });
+    describe('when not sucessfull', () => {
+      beforeEach(() => clearMocks());
+
+      it("should throw an error when passwords don't match", async () => {
+        jest.spyOn(accountRepositoryMock, 'getByEmail').mockResolvedValue({
+          id: 4,
+          email: 'rocco@email.com',
+          fullName: 'Rocco Anderson',
+          birthDate: '2000-03-05T03:00:00.000Z',
+          hashedPassword: 'hashedPassword',
+          balance: 0,
+        });
+
+        jest
+          .spyOn(bcrypt, 'compare')
+          .mockImplementation(() => Promise.resolve(false));
+
+        await expect(
+          authService.signIn({
+            email: 'rocco@email.com',
+            password: 'wrongPassword',
+          }),
+        ).rejects.toThrow(new Error('Email or password invalid'));
+      });
+
+      it('should throw an error when account does not exists', async () => {
+        jest.spyOn(accountRepositoryMock, 'getByEmail').mockResolvedValue(null);
+
+        await expect(
+          authService.signIn({
+            email: 'not_existent_account@email.com',
+            password: 'password',
+          }),
+        ).rejects.toThrow(new Error('Account does not exists.'));
+      });
+
+      it('should throw an error when "email" or "password are not passed as arguments', async () => {
+        await expect(
+          authService.signUp({
+            fullName: 'Rocco Anderson',
+            birthDate: '03/05/2000',
+            password: 'password',
+          } as never),
+        ).rejects.toThrow(new Error('email should not be empty'));
+
+        await expect(
+          authService.signUp({
+            email: 'rocco@email.com',
+            fullName: 'Rocco Anderson',
+            birthDate: '03/05/2000',
+          } as never),
+        ).rejects.toThrow(new Error('password should not be empty'));
+      });
+
+      it('should throw an error when email format is invalid', async () => {
+        await expect(
+          authService.signUp({
+            email: 'roccoemail.com',
+            fullName: 'Rocco Anderson',
+            birthDate: '03/05/2000',
+            password: 'password',
+          }),
+        ).rejects.toThrow(new Error('email must be an email'));
+
+        await expect(
+          authService.signUp({
+            email: 'rocco@emailcom',
+            fullName: 'Rocco Anderson',
+            birthDate: '03/05/2000',
+            password: 'password',
+          }),
+        ).rejects.toThrow(new Error('email must be an email'));
+      });
+    });
+  });
 });
