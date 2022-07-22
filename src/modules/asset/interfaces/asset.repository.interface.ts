1cce1016492af13379da7783b4e355186a439dad
moraes <moraeslua66@gmail.com>
2022-07-21 22:41:21 -0300

    feat(assetRepository): get all method implementation and interface

--- src/modules/asset/interfaces/asset.repository.interface.ts
+++ src/modules/asset/interfaces/asset.repository.interface.ts
@@ -21,7 +21,23 @@ export interface IUpdateAssetAmountResult {
   amount: number;
 }
 
+export interface IGetAllData {
+  limit: number;
+  offset: number;
+}
+
+export interface IGetAllResponse {
+  id: number;
+  symbol: string;
+  companyName: string;
+  price: number;
+  exchange: string;
+  exchangeShortName: string;
+  amount: number;
+}
+
 export interface IAssetRepository {
+  getAll(data: IGetAllData): Promise<IGetAllResponse[]>;
   findById(data: IGetAssetByIdData): Promise<IGetAssetByIdResult>;
   updateAmount(data: IUpdateAssetAmountData): Promise<IUpdateAssetAmountResult>;
 }
--- src/modules/asset/repository/prisma.asset.repository.ts
+++ src/modules/asset/repository/prisma.asset.repository.ts
@@ -1,6 +1,8 @@
 import { PrismaClient } from '@prisma/client';
 import {
   IAssetRepository,
+  IGetAllData,
+  IGetAllResponse,
   IGetAssetByIdData,
   IGetAssetByIdResult,
   IUpdateAssetAmountData,
@@ -9,6 +11,16 @@ import {
 
 export class PrismaAssetRepository implements IAssetRepository {
   constructor(private client: PrismaClient) {}
+  public async getAll(data: IGetAllData): Promise<IGetAllResponse[]> {
+    const { limit, offset } = data;
+
+    const assets = await this.client.asset.findMany({
+      skip: offset,
+      take: limit,
+    });
+
+    return assets;
+  }
 
   public async updateAmount(
     data: IUpdateAssetAmountData,
