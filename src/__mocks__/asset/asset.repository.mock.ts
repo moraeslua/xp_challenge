ef953d8eb2fcbcd579e77bff45d6d1a333bc4aaa
moraes <moraeslua66@gmail.com>
2022-07-21 22:45:00 -0300

    feat(mocks): update asset repository mock

--- src/__mocks__/asset/asset.repository.mock.ts
+++ src/__mocks__/asset/asset.repository.mock.ts
@@ -1,6 +1,7 @@
 import { IAssetRepository } from 'src/modules/asset/interfaces/asset.repository.interface';
 
 export class AssetRepositoryMock implements IAssetRepository {
+  getAll = jest.fn();
   findById = jest.fn();
   updateAmount = jest.fn();
 }
