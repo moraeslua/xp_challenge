33d6fd4c5d7cfa13b3a8289669c2246a768cf239
moraes <moraeslua66@gmail.com>
2022-07-23 18:33:14 -0300

    chore: move app from main.ts to app.ts

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+import express from 'express';
+import cors from 'cors';
+import { appRoutes } from './app.routes';
+import { errorHandler } from './middlewares/error-handler';
+import 'express-async-errors';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(appRoutes);
+
+app.use(errorHandler);
+
+export default app;
--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,5 @@
-import * as dotenv from 'dotenv';
-dotenv.config();
-import express from 'express';
-import cors from 'cors';
-import { appRoutes } from './app.routes';
-import { errorHandler } from './middlewares/error-handler';
-import 'express-async-errors';
+import app from './app';
 
-const app = express();
+const PORT = process.env.PORT;
 
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.use(appRoutes);
-
-app.use(errorHandler);
-
-app.listen(3002, () => console.log('server listening on port 3002'));
+app.listen(PORT, () => console.log(`server listening on port ${PORT}`));
